543dc17a50095fbd1b4e705f7b2e8698fbc2932d
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-11 16:50:35 +0530

    updated

--- day1/customerservice/customer/models.py
+++ day1/customerservice/customer/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    account_no = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=10)
+
+
+class Address(models.Model):
+    door_no=models.CharField(max_length=5)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='addresses')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
