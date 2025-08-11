eff4c30c95a2c945e16f607cc1bcff90053c98ed
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-11 16:37:25 +0530

    updated

--- day1/customerservice/customer_api/settings.py
+++ day1/customerservice/customer_api/settings.py
@@ -92,9 +92,14 @@ WSGI_APPLICATION = 'customer_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'niitcustomerdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
 }
 
 
