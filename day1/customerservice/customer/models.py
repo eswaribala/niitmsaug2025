9a404fec21e450430b3d873930b8d735e102cf0f
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-11 17:00:35 +0530

    updated

--- day1/customerservice/customer/models.py
+++ day1/customerservice/customer/models.py
@@ -23,3 +23,31 @@ class Address(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='addresses')
     def __str__(self):
         return f"{self.door_no}, {self.street}, {self.city}, {self.state}, {self.zip_code}, {self.customer}"
+
+class Gender(models.TextChoices):
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+
+class CompanyType(models.TextChoices):
+    PRIVATE = 'P'
+    PUBLIC = 'U'
+    GOVERNMENT = 'G'
+
+class Individual(Customer):
+    gender = models.TextField(
+        choices=Gender.choices,
+        default=Gender.MALE
+    )
+    date_of_birth = models.DateField()
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.email}, {self.phone}, {self.account_no}, {self.created_at}, {self.gender}, {self.date_of_birth}"
+    
+class Corporate(Customer):
+
+    company_type = models.TextField(
+        choices=CompanyType.choices,
+        default=CompanyType.PRIVATE
+    )
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.email}, {self.phone}, {self.account_no}, {self.created_at}, {self.company_name}, {self.company_type}"
