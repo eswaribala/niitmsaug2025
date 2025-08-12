5a28ddfe3317ff6bc5bdd16863ad683527b8b476
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-12 18:58:19 +0530

    updated

---
+++ customerapiexternalconfig/src/main/java/com/niit/customerapi/configurations/DbConfiguration.java
@@ -0,0 +1,7 @@
+package com.niit.customerapi.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DbConfiguration {
+}
---
+++ customerapiexternalconfig/src/main/java/com/niit/customerapi/configurations/VaultConfiguration.java
@@ -0,0 +1,4 @@
+package com.niit.customerapi.configurations;
+
+public class VaultConfiguration {
+}
