b0087ec86aeac2a1819b0eb5756ce6d3b6c6f430
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-12 19:09:56 +0530

    updated

--- customerapiexternalconfig/src/main/java/com/niit/customerapi/configurations/DbConfiguration.java
+++ customerapiexternalconfig/src/main/java/com/niit/customerapi/configurations/DbConfiguration.java
@@ -1,7 +1,42 @@
 package com.niit.customerapi.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
+@EnableConfigurationProperties(VaultConfiguration.class)
 public class DbConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String postgresurl;
+    @Value("${spring.datasource.driver-class-name}")
+    private String postgresDriver;
+
+    private DataSourceBuilder dataSourceBuilder;
+
+    private final VaultConfiguration  vaultConfiguration;
+
+    public DbConfiguration(VaultConfiguration vaultConfiguration) {
+        this.vaultConfiguration = vaultConfiguration;
+    }
+
+
+    @Bean
+    public DataSource getDataSource() {
+
+        dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(postgresDriver);
+        dataSourceBuilder.url(postgresurl);
+        dataSourceBuilder.username(vaultConfiguration.getPostgresusername());
+        dataSourceBuilder.password(vaultConfiguration.getPostgrespassword());
+        return dataSourceBuilder.build();
+
+    }
+
+
 }
--- customerapiexternalconfig/src/main/java/com/niit/customerapi/configurations/VaultConfiguration.java
+++ customerapiexternalconfig/src/main/java/com/niit/customerapi/configurations/VaultConfiguration.java
@@ -1,4 +1,11 @@
 package com.niit.customerapi.configurations;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties
 public class VaultConfiguration {
+    private String postgresusername;
+    private String postgrespassword;
 }
