08d922c612e38b97bd2ff82dd107c07e5133e5df
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-13 14:37:45 +0530

    updated

--- bankingapi/bankingapi/src/main/java/com/db/bankingapi/controllers/IndividualController.java
+++ bankingapi/bankingapi/src/main/java/com/db/bankingapi/controllers/IndividualController.java
@@ -10,7 +10,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -49,7 +49,7 @@ public class IndividualController {
     }
 
     @GetMapping("/v1.0")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+
     public List<Individual> getAllIndividuals(){
        return  individualService.getAllIndividuals();
     }
