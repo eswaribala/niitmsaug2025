329766ed92ecef330f2fe2144fd49bb018703c3a
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-14 21:48:47 +0530

    updated

--- accountapi/src/main/java/com/niit/accountapi/services/ConsumerServiceImpl.java
+++ accountapi/src/main/java/com/niit/accountapi/services/ConsumerServiceImpl.java
@@ -3,6 +3,7 @@ package com.niit.accountapi.services;
 import com.google.gson.Gson;
 import com.niit.accountapi.dtos.CustomerDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,12 @@ import org.springframework.stereotype.Service;
 public class ConsumerServiceImpl implements ConsumerService {
 
     @Override
+    @KafkaHandler(isDefault = true)
     public void getData(String json) {
         log.info("Consumer service received data: {}", json);
 
-        CustomerDTO  customerDTO = new Gson().fromJson(json, CustomerDTO.class);
-        log.info("CustomerDTO: {}", customerDTO);
+        //CustomerDTO  customerDTO = new Gson().fromJson(json, CustomerDTO.class);
+      //  log.info("CustomerDTO: {}", customerDTO);
 
 
     }
